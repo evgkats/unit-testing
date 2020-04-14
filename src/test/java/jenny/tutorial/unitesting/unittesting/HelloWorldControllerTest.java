4c527fa3177cf04ca188362eccc76a7413abffc8
Evgenia Katsada <evgenia.katsada@wirecard.com>
2020-04-14 17:39:50 +0300

    Use ResponseMatchers to check status and content

--- src/test/java/jenny/tutorial/unitesting/unittesting/HelloWorldControllerTest.java
+++ src/test/java/jenny/tutorial/unitesting/unittesting/HelloWorldControllerTest.java
@@ -8,8 +8,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(HelloWorldController.class)
 class HelloWorldControllerTest {
@@ -23,9 +26,14 @@ class HelloWorldControllerTest {
         RequestBuilder request = MockMvcRequestBuilders
                 .get("/hello-world")
                 .accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(request).andReturn();
+        MvcResult result = mockMvc
+                .perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"))
+                .andReturn();
 
         // verify "Hello World"
-        assertEquals("Hello World", result.getResponse().getContentAsString());
+        //assertEquals("Hello World", result.getResponse().getContentAsString());
+        // assert no longer needed for such a simple response
     }
 }
