f899d7ede14cc2bdb38dd25f138597ce18bd49b1
Evgenia Katsada <evgkatsada@gmail.com>
2020-04-20 20:27:37 +0300

    Create Item entity and populate data using a new file in resources data.sql

--- src/main/java/jenny/tutorial/unitesting/unittesting/model/Item.java
+++ src/main/java/jenny/tutorial/unitesting/unittesting/model/Item.java
@@ -1,10 +1,19 @@
 package jenny.tutorial.unitesting.unittesting.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
 public class Item {
+    @Id
     private int id;
     private String name;
     private int price;
     private int quantity;
+
+    @Transient
+    private int value;
 
     public Item(int id, String name, int price, int quantity) {
         this.id = id;
@@ -43,5 +52,13 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 }
