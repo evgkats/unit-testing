4bf5d26131e92a245c3fdc6a7c3bfc296a383205
Evgenia Katsada <evgkatsada@gmail.com>
2020-06-28 12:57:44 +0300

    Use @MockBean to mock out dependencies

--- src/test/java/jenny/tutorial/unitesting/unittesting/ItemControllerIntegrationTest.java
+++ src/test/java/jenny/tutorial/unitesting/unittesting/ItemControllerIntegrationTest.java
@@ -1,11 +1,18 @@
 package jenny.tutorial.unitesting.unittesting;
 
+import jenny.tutorial.unitesting.unittesting.data.ItemRepository;
+import jenny.tutorial.unitesting.unittesting.model.Item;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ItemControllerIntegrationTest {
@@ -13,9 +20,21 @@ public class ItemControllerIntegrationTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @MockBean
+    private ItemRepository repository;
+
     @Test
     public void contextLoads() throws JSONException {
         String response = this.restTemplate.getForObject("/all-items-from-database", String.class);
         JSONAssert.assertEquals("[{id:10001}, {id:10002}, {id:10003}]", response, false);
+    }
+
+    @Test
+    public void contextLoadsWithMock() throws JSONException {
+
+        when(repository.findAll()).thenReturn(Arrays.asList(new Item(1, "test", 10, 10)));
+
+        String response = this.restTemplate.getForObject("/all-items-from-database", String.class);
+        JSONAssert.assertEquals("[{id:1}]", response, false);
     }
 }
